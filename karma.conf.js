f5198beec5207e737f8dc8e6d1646ebe43fc4c61
SimHub <lackmann.simon@gmail.com>
2018-01-12 14:03:32 +0100

    modified file

--- karma.conf.js
+++ karma.conf.js
@@ -68,8 +68,8 @@ module.exports = function (config) {
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         // browsers: ['PhantomJS'],
-        browsers: ['Chrome'], // You may use 'ChromeCanary', 'Chromium' or any other supported browser
-        // browsers: ['ChromeHeadless'],
+        // browsers: ['Chrome'], // You may use 'ChromeCanary', 'Chromium' or any other supported browser
+        browsers: ['ChromeHeadless'],
 
         // customLaunchers: {
         //     Chrome_travis_ci: {
